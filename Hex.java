a8c234a34de9e72b7124de078cc08bf77f2a5d55
Rakib Bhuiyan <bhuiyanmohammadrakibulislam@gmail.com>
2026-09-20 19:17:13 -0400

    Add Hex

---
+++ Hex.java
@@ -0,0 +1,9 @@
+public class Hex {
+    public static void main(String[] args) {
+        int number = 45;
+
+        System.out.println(Integer.toBinaryString(number));
+        System.out.println(number);
+System.out.println(Integer.toHexString(number));
+    }
+}
